e38b2894dd1b346d27e814ef0252e0eec3389784
Suhaila Japar <suhaila@besquare.com.my>
2022-03-13 11:39:35 +0800

    revert

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const walletRouter = require("./routes/wallet.route");
 const transactionRouter = require("./routes/transaction.route");
 const fileRouter = require("./routes/file.route");
 
-const corsOptions = {
-  origin: "http://159.223.55.216", //included origin as true
-  credentials: true, //included credentials as true
-};
-
 const app = express();
 app.use(
   fileUpload({
@@ -21,7 +16,7 @@ app.use(
     },
   })
 );
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
