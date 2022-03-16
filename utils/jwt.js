fec05fb338b3f7ec1184527047b704b1e8a6f2e2
Suhaila Japar <suhaila@besquare.com.my>
2022-03-16 23:20:08 +0800

    jwt expiry fix

--- utils/jwt.js
+++ utils/jwt.js
@@ -3,7 +3,7 @@ const { jwt_secret, jwt_expiry } = require("./jwt_config");
 
 const createToken = (email, login_id) => {
   const accessToken = sign({ email, login_id }, jwt_secret, {
-    expiresIn: jwt_expiry,
+    expiresIn: parseInt(jwt_expiry),
   });
   return accessToken;
 };
